7e9616d64206d584b5c5f8f298e450068da2b683
Lukasz Janyst <lukasz@jany.st>
2017-08-19 21:44:30 +0200

    Fix or suppress pylint issues in twist_controller

--- ros/src/twist_controller/lowpass.py
+++ ros/src/twist_controller/lowpass.py
@@ -1,16 +1,31 @@
+#-------------------------------------------------------------------------------
+# Author: xxx xxx <xxx@xxx.xxx>
+# Date:   xx.xx.xx
+#-------------------------------------------------------------------------------
+
+"""
+STUB
+"""
 
 class LowPassFilter(object):
+    """
+    STUB
+    """
     def __init__(self, tau, ts):
         self.a = 1. / (tau / ts + 1.)
-        self.b = tau / ts / (tau / ts + 1.);
+        self.b = tau / ts / (tau / ts + 1.)
 
         self.last_val = 0.
         self.ready = False
 
     def get(self):
+        """STUB"""
         return self.last_val
 
     def filt(self, val):
+        """
+        STUB
+        """
         if self.ready:
             val = self.a * val + self.b * self.last_val
         else:
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,35 @@
+#-------------------------------------------------------------------------------
+# Author: xxx xxx <xxx@xxx.xxx>
+# Date:   xx.xx.xx
+#-------------------------------------------------------------------------------
+
+"""
+STUB
+"""
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
+#pylint: disable=fixme
 
 class Controller(object):
+    """
+    STUB
+    """
     def __init__(self, *args, **kwargs):
+        #pylint: disable=unused-argument
+        #pylint: disable=no-self-use
+
         # TODO: Implement
         pass
 
     def control(self, *args, **kwargs):
+        #pylint: disable=unused-argument
+        #pylint: disable=no-self-use
+
+        """
+        STUB
+        """
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
         return 1., 0., 0.
