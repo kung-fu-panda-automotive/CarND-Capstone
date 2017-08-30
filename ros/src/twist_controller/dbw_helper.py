19af74dcb377d91923b39a77c71695e5b878c53b
Kostas Oreopoulos <kostas.oreopoulos@gmail.com>
2017-08-30 11:15:09 -0700

    fix: Remove matplotlib and functions using it

--- ros/src/twist_controller/dbw_helper.py
+++ ros/src/twist_controller/dbw_helper.py
@@ -6,10 +6,9 @@
 """
 A collection of helper functions for dbw node
 """
-# pylint: disable=import-error
+
 from math import sqrt, cos, sin
 import numpy as np
-import matplotlib.pyplot as plt
 import tf
 
 # pylint: disable=invalid-name
@@ -38,7 +37,7 @@ def eucleidian_distance(x0, y0, x1, y1):
     return sqrt(pow(x0 - x1, 2) + pow(y0 - y1, 2))
 
 
-def distance2parabola(coefficients, x, y, plot=False):
+def distance2parabola(coefficients, x, y):
     """
     Calculates the distance of a point from a parabola defined
     by its polynomial coefficients.
@@ -54,7 +53,7 @@ def distance2parabola(coefficients, x, y, plot=False):
         left (int) : 1 if the point is on the left of the curve as we "walk" the curve
                      from negative to positive x's. -1 if on the right
     """
-    #pylint: disable=too-many-locals
+    # pylint: disable=too-many-locals
     a, b, c = coefficients
     p0 = 2 * a * a
     p1 = 3 * a * b
@@ -68,48 +67,10 @@ def distance2parabola(coefficients, x, y, plot=False):
     x_ = np.real(roots[np.isreal(roots)])[0]
     y_ = np.polyval(coefficients, x_)
 
-    if plot:
-        plot_distance(coefficients, x, y, x_, y_)
-
     distance = eucleidian_distance(x, y, x_, y_)
     left = -1 if y_ < y else 1
 
     return distance, left, x_, y_
-
-
-def plot_distance(coefficients, x_0, y_0, x_, y_):
-    """
-    Args:
-        coefficients (list): the coefficients of the parabola
-        x_0 (float): the x coordinate of the point outside the parabola we want distance
-        y_0 (float): the y coordinate of the point outside the parabola we want distance
-        x_ (float): the x coordinate of the point on the parabola closest to (x_0,y_0)
-        y_ (float): the y coordinate of the point on the parabola closest to (x_0,y_0)
-    """
-    def f(x):
-        """
-        Evaluate polynomial
-        """
-        return np.polyval(coefficients, x)
-
-    X = [x_, y_]
-
-    x = np.linspace(-15, 15, 100)
-
-    plt.plot(x, f(x), 'r-', label='f(x)')
-    plt.plot(x_0, y_0, 'bo', label='point')
-    plt.plot([x_0, X[0]], [y_0, X[1]], 'b-', label='shortest distance')
-    delta = 2.0
-    plt.plot([X[0], X[0] + delta],
-             [X[1], X[1] + (2.0 * coefficients[0] * X[0] +
-                            coefficients[1]) * delta],
-             'g-', label='tangent')
-
-    plt.axis('equal')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.legend(loc='best')
-    plt.show()
 
 
 def shift_and_rotate_waypoints(pose, waypoints, points_to_use=None):
@@ -136,7 +97,6 @@ def shift_and_rotate_waypoints(pose, waypoints, points_to_use=None):
 
     if points_to_use is None:
         points_to_use = len(waypoints)
-
 
     for i in range(points_to_use):
 
@@ -192,13 +152,9 @@ def cte(pose, waypoints):
     Returns:
         cte (float) : the cross track error (signed)
     """
-    x_coords, y_coords = shift_and_rotate_waypoints(pose, waypoints, POINTS_TO_FIT)
+    x_coords, y_coords = shift_and_rotate_waypoints(
+        pose, waypoints, POINTS_TO_FIT)
     coefficients = np.polyfit(x_coords, y_coords, 3)
     distance = np.polyval(coefficients, 2.0)
 
     return distance
-
-
-if __name__ == '__main__':
-    cf = [-8.34706486e-04, -5.37658333e-01, -4.88838535e+00]
-    print distance2parabola(cf, 0, 0, plot=True)
