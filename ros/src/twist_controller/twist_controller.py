71e2f98e440d73102ce7ac481a458937a41f5288
Kostas Oreopoulos <kostas.oreopoulos@gmail.com>
2017-08-30 01:01:51 -0700

    feature: Finish Twist Controller
    - enable filters
    - tune pid parameters
    - remove print statements

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -10,7 +10,6 @@ Implement a simple car controller
 
 import rospy
 import pid
-import yaw_controller
 import lowpass
 
 GAS_DENSITY = 2.858
@@ -44,48 +43,45 @@ class Controller(object):
         min_speed = 0.0
 
         # create controllers
-        self.yaw_controller = yaw_controller.YawController(
-            wb, sr, min_speed, ml, ms)
-        self.throttle_pid = pid.PID(kp=0.5, ki=0.0, kd=0.0, mn=mn, mx=mx)
-        self.steer_pid = pid.PID(kp=1.0, ki=0.0, kd=0.5, mn=-ms, mx=ms)
+        self.throttle_pid = pid.PID(kp=1.0, ki=0.02, kd=0.0, mn=mn, mx=mx)
+        self.steer_pid = pid.PID(kp=0.2, ki=0.001, kd=0.5, mn=-ms, mx=ms)
 
         # create lowpass filters
-        self.throttle_filter = lowpass.LowPassFilter(tau=0.10, ts=1.0)
-        self.steer_filter = lowpass.LowPassFilter(tau=0.10, ts=1.0)
+        self.throttle_filter = lowpass.LowPassFilter(tau=0.10, ts=0.90)
+        self.steer_filter = lowpass.LowPassFilter(tau=0.10, ts=0.90)
 
         # init timestamp
         self.timestamp = rospy.get_time()
 
-    def control(self, target_velocity, current_velocity, cte, dbw_enabled):
+    def control(self, vel_error, cte, dbw_enabled):
         """
         Given then target linear and angular velocities, as well as the current velocity
         calculate the CTE and pass them to the corresponding PID's
 
         Args:
-             target_velocity (float) : The target linear velocity in the x axis
-             target_angular_velocity (float): The target angular velocity in the z axis
+             vel_error (float) : Velocity Error
              current_velocity (float) : The current linear velocity in the x axis
+             cte (float): Cross Track Error
              dbw_enabled (bool) : If True: auto pilot is on. If False: Manual driving
         """
         new_timestamp = rospy.get_time()
         duration = new_timestamp - self.timestamp
         sample_time = duration + 1e-6  # to avoid division by zero
+
         self.timestamp = new_timestamp
         if dbw_enabled:
+            # initialize values
             brake = 0
             throttle = 0
             # calculate new steering angle
-            steering_angle = -self.steer_pid.step(cte, sample_time)
-            #steering_angle = self.steer_filter.filt(steering_angle)
-            print("CTE pid: {}".format(cte))
-            print("Steering PID: {}".format(steering_angle))
+            steering_angle = self.steer_pid.step(cte, sample_time)
+            steering_angle = self.steer_filter.filt(steering_angle)
             # caclulate throttle and brake
-            throttle = self.throttle_pid.step(
-                target_velocity - current_velocity, sample_time)
-            #throttle = self.throttle_filter.filt(throttle)
+            throttle = self.throttle_pid.step(vel_error, sample_time)
+            throttle = self.throttle_filter.filt(throttle)
             # convert to the expected format
             if throttle < 0:
-                brake = -throttle
+                brake = throttle
                 throttle = 0
             return throttle, brake, steering_angle
         # dbw is not enabled (manual override) so resetting pid's and filters
