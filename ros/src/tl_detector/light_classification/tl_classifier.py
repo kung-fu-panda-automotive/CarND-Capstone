0c7ae4676cd993af76d6926d08be9ef079e99132
Alexey <alexey.simonov@gmail.com>
2017-09-05 21:36:56 +0100

    tensorflow implementation of TLClassifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,32 +1,73 @@
 #-------------------------------------------------------------------------------
-# Author: xxx xxx <xxx@xxx.xxx>
-# Date:   xx.xx.xx
+# Author: Alexey Simonov <alexey.simonov@gmail.com>
+# Date:   05.09.17
 #-------------------------------------------------------------------------------
 
 """
-DUMMY DOCSTRING - TO BE iMPLEMENTED
+Traffic Light Classifier for Carla
+
+This is the class in the ROS project framework provided by Udacity.
+
+Actual implementation is delegated to TLClassifierCNN, a convolutional net classifier implemented
+with tensorflow.
+
+The development/reseach/training repo for TLClassifierCNN is
+https://github.com/asimonov/Bosch-TL-Dataset. Check it out to train and update model.
+
+TODO:
+  * TLClassifierCNN opens tensorflow session. How shall we size memory for that?
+    Shall we place it on CPU? It should not make much difference for inference on single-image
+    batches but may avoid runtime issues.
 """
 
 from styx_msgs.msg import TrafficLight
+from tl_classifier_cnn import TLLabelConverter, TLClassifierCNN
+import cv2
+import numpy as np
 
 class TLClassifier(object):
-    #pylint: disable=too-few-public-methods
+  """
+  Traffic Lights classifier
+
+  Creates an instance of TLClassifierCNN, which creates a tensorflow session.
+  Loads pre-trained model.
+
+  Attributes:
+    _label_converter: TLLabelConverter to convert string labels to other representations
+    _cnn_classifier: TLClassifierCNN for actual classification
+    _classifier_image_size: tuple with the image size the TLClassifierCNN expects
+  """
+  def __init__(self):
+    self._label_converter = TLLabelConverter()
+    self._classifier_image_size = (32,32)
+    self._cnn_classifier = TLClassifierCNN()
+    # model path below is relative to tl_detector/ node folder
+    # (see cwd="node" in tl_detector.launch )
+    self._cnn_classifier.load_model('light_classification/model')
+
+  def get_classification(self, image):
+    """Determines the color of the traffic light in the image
+
+    Resizes image and uses TLClassifierCNN to determine the light.
+
+    Args:
+        image (cv::Mat): image containing the traffic light. Can be arbitrary size, but expected to
+                         have BGR channels, usual OpenCV format.
+
+    Returns:
+        int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+
     """
-    DUMMY DOCSTRING - TO BE IMPLEMENTED
-    """
-    def __init__(self):
-        pass
+    resized = cv2.resize(image, self._classifier_image_size, interpolation=cv2.INTER_LINEAR)
+    assert (resized.shape==(32,32,3))
+    labels, probabilities = self._cnn_classifier.predict(np.array([resized]), batch_size=1)
 
-    def get_classification(self, image):
-        #pylint: disable=unused-argument
-        #pylint: disable=no-self-use
-        """Determines the color of the traffic light in the image
+    result = TrafficLight.UNKNOWN
+    if labels[0]=='red':
+      result = TrafficLight.RED
+    elif labels[0]=='green':
+      result = TrafficLight.GREEN
+    elif labels[0]=='yellow':
+      result = TrafficLight.YELLOW
 
-        Args:
-            image (cv::Mat): image containing the traffic light
-
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
-        """
-        return TrafficLight.UNKNOWN
+    return result
