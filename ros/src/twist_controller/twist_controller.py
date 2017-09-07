906c7c7e2b9b9d7a582c70ab33698c0229e1aeee
Kostas Oreopoulos <kostas.oreopoulos@gmail.com>
2017-09-07 13:36:16 -0700

    feat: Change filters and pid parameters
    - filters now work on errors and not on the pid results
    - fixed braking factor
    - fixed throttle pid coefficients

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -44,11 +44,11 @@ class Controller(object):
 
         # create controllers
         self.throttle_pid = pid.PID(kp=1.0, ki=0.02, kd=0.0, mn=mn, mx=mx)
-        self.steer_pid = pid.PID(kp=0.088, ki=0.003, kd=0.15, mn=-ms, mx=ms)
+        self.steer_pid = pid.PID(kp=0.5, ki=0.003, kd=0.25, mn=-ms, mx=ms)
 
         # create lowpass filters
         self.throttle_filter = lowpass.LowPassFilter(tau=0.10, ts=0.90)
-        self.steer_filter = lowpass.LowPassFilter(tau=0.00, ts=1.00)
+        self.steer_filter = lowpass.LowPassFilter(tau=0., ts=1.)
 
         # init timestamp
         self.timestamp = rospy.get_time()
@@ -72,15 +72,16 @@ class Controller(object):
             # initialize values
             brake = 0.
             throttle = 0.
+            # filter (smooth) errors
+            cte = self.steer_filter.filt(cte)
+            vel_error = self.throttle_filter.filt(vel_error)
             # calculate new steering angle
             steering_angle = self.steer_pid.step(cte, sample_time)
-            steering_angle = self.steer_filter.filt(steering_angle)
             # caclulate throttle and brake
             throttle = self.throttle_pid.step(vel_error, sample_time)
-            throttle = self.throttle_filter.filt(throttle)
             # convert to the expected format
             if throttle < 0.:
-                brake = throttle
+                brake = 1000 * abs(throttle)
                 throttle = 0.
             return throttle, brake, steering_angle
         # dbw is not enabled (manual override) so resetting pid's and filters
