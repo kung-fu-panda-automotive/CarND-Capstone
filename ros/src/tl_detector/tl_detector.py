86dab25703494424fa3358f2276b7be06dc532ee
Mithi <mithi.sevilla@gmail.com>
2017-09-07 18:50:29 +0800

    Add temporary tl detector for testing

---
+++ ros/src/tl_detector/tl_detector.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+""" This is where we will write the real Traffic Lights Detector
+Use fake_tl_detector for now for testing
+check out tl_detector_template.py
+as provided by Udacity for ideas to implement this
+"""
+
+import rospy
+from fake_tl_detector import TLDetector # For testing for now
+
+if __name__ == '__main__':
+    try:
+        TLDetector()
+    except rospy.ROSInterruptException:
+        rospy.logerr('Could not start traffic node.')
