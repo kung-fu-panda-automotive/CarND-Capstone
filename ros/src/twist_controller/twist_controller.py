e29f6fe958428efc2980bd503b605e492ed85298
Kostas Oreopoulos <kostas.oreopoulos@gmail.com>
2017-09-07 14:52:04 -0700

    feat: Integrade the speed controller in existing code
    - remove throttle and brake pid's from previous controller
    - create speed controller in dbw_node
    -  use it to get throttle and brake

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -33,33 +33,22 @@ class Controller(object):
 
     """
 
-    def __init__(self, **kwargs):
-        mn = kwargs.get('min_acc', 0.0)  # decel_limit
-        mx = kwargs.get('max_acc', 0.0)  # accel_limit
-        # wb = kwargs.get('wheel_base', 0.0)  # wheel_base
-        # sr = kwargs.get('steer_ratio', 0.0)  # steer_ratio
-        # ml = kwargs.get('max_lat_acc', 0.0)  # max_lat_accel
-        ms = kwargs.get('max_steer_angle', 0.0)  # max_steer_angle
-        # min_speed = 0.0
+    def __init__(self, max_steer_angle):
+        ms = max_steer_angle  # max_steer_angle
 
         # create controllers
-        self.throttle_pid = pid.PID(kp=1.0, ki=0.02, kd=0.0, mn=mn, mx=mx)
         self.steer_pid = pid.PID(kp=0.5, ki=0.003, kd=0.25, mn=-ms, mx=ms)
-
         # create lowpass filters
-        self.throttle_filter = lowpass.LowPassFilter(tau=0.10, ts=0.90)
         self.steer_filter = lowpass.LowPassFilter(tau=0., ts=1.)
-
         # init timestamp
         self.timestamp = rospy.get_time()
 
-    def control(self, vel_error, cte, dbw_enabled):
+    def control(self, cte, dbw_enabled):
         """
         Given then target linear and angular velocities, as well as the current velocity
         calculate the CTE and pass them to the corresponding PID's
 
         Args:
-             vel_error (float) : Velocity Error
              cte (float): Cross Track Error
              dbw_enabled (bool) : If True: auto pilot is on. If False: Manual driving
         """
@@ -69,25 +58,13 @@ class Controller(object):
 
         self.timestamp = new_timestamp
         if dbw_enabled:
-            # initialize values
-            brake = 0.
-            throttle = 0.
             # filter (smooth) errors
             cte = self.steer_filter.filt(cte)
-            vel_error = self.throttle_filter.filt(vel_error)
             # calculate new steering angle
             steering_angle = self.steer_pid.step(cte, sample_time)
-            # caclulate throttle and brake
-            throttle = self.throttle_pid.step(vel_error, sample_time)
-            # convert to the expected format
-            if throttle < 0.:
-                brake = 1000 * abs(throttle)
-                throttle = 0.
-            return throttle, brake, steering_angle
+            return steering_angle
         # dbw is not enabled (manual override) so resetting pid's and filters
-        self.throttle_pid.reset()
         self.steer_pid.reset()
-        self.throttle_filter.last_val = 0.0
         self.steer_filter.last_val = 0.0
 
-        return 0.0, 0.0, 0.0
+        return 0.0
